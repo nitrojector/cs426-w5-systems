b8c0a58e80eb9958b03ad45ca8d20976e5ddb6e3
wyvern <nitrojector@takina.io>
2026-02-13 15:45:23 -0800

    feat: background

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    public float velocity = -2.0f;
+    public float yPos = 0.0f;
+
+    private void Update()
+    {
+        yPos += velocity * Time.deltaTime;
+        yPos %= 7.4f;
+        Vector3 offset = new(0.0f, yPos, 0.0f);
+        transform.position = offset;
+    }
+}
