d3c6d1d9cdeb2a2c2f3bbee1a2fa813cfa06c3bd
wyvern <nitrojector@takina.io>
2026-02-17 21:46:40 -0800

    feat: tweaks

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -2,7 +2,10 @@
 {
     public const float BackgroundYMvmtVel = -2.0f;
     public const float DespawnY = -8.0f;
+    public const float PlayerDeathY = -6.0f;
     public const float PlayerDeathScale = 0.8f;
+
+    public const float SpecialCakeCalorieValuePerc = 0.33f;
 
     public const float PlayerCalorieBurnRateRegular = 0.08f;
     public const float PlayerCalorieBurnRateStrafe = 0.16f;
